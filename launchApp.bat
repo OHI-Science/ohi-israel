5b05e4243093d4947fc046a4c69eff8f4ad4dcab
Ben Best <bbest@nceas.ucsb.edu>
2014-06-11 10:27:02 -0700

    updated Win path to Rscript.exe

--- launchApp.bat
+++ launchApp.bat
@@ -1,3 +1,3 @@
 REM on Microsoft Windows (adjust the path to R.exe as needed)
-"C:\Program Files\R\R-3.0.2\bin\Rscript.exe" "%CD%\launchApp_code.R"
+"C:\R-3.1.0\bin\Rscript.exe" "%CD%\launchApp_code.R"
 PAUSE
