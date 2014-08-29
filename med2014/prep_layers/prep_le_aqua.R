520aa5afa624690aca9b4625e5b63366d3f969d3
jules32 <jstewart@nceas.ucsb.edu>
2014-08-29 14:28:00 -0700

    progress in creating LE layers (not complete)

---
+++ med2014/prep_layers/prep_le_aqua.R
@@ -0,0 +1,45 @@
+# prep_le_aqua.r
+
+# prep agriculture/aquaculture jobs + wages data:
+# 'economies data.csv' is created from Anat's 'economies data.xlsx'
+
+
+# setup
+file_in  = file.path(dir_prep, 'economies data.csv')
+
+
+# read in file
+f = read.csv(file_in); head(f); sapply(f, class)
+
+## combine coastal cities:
+# sum jobs and average wages across coastal cities into districts (Herzlia + Netanya --> Center)
+
+jobs_by_2rgn = f %>%
+  group_by(rgn_id, year) %>%
+  summarize(hotel_jobs_n               = sum(Number.of.hotel.Jobs),
+            wages_tourism_monthly_mean = mean(Mean.monthly.wages.Tourism))
+head(jobs_by_2rgn); summary(jobs_by_2rgn)
+
+## combine regions:
+# sum jobs and average wages across regions into Israel (Tel aviv + Center --> Israel)
+
+jobs_by_israel = jobs_by_2rgn %>%
+  group_by(year) %>%
+  summarize(hotel_jobs_n               = sum(hotel_jobs_n),
+            wages_tourism_monthly_mean = mean(wages_tourism_monthly_mean)) %>%
+    mutate(rgn_id = 0) %>%
+  select(rgn_id, year, hotel_jobs_n, wages_tourism_monthly_mean)
+head(jobs_by_israel); summary(jobs_by_israel)
+
+# Option 1. 
+write.csv(jobs_by_israel, file.path(dir_prep,'hotel_jobs_wrong_tmp.csv'), row.names=F, na='')
+# by hand save to hotel_jobs_wrong.csv after repeating for rgns 1:6. 
+# hotel_jobs_wrong.csv will still be incomplete because you will now have 6x total jobs in Israel, and wages for rgns that you know have no tourism. So will either need to apply a modifier (coastal population?) to this file, or, take the following approach and then gapfill 3 regions:
+
+# Option 2. 
+write.csv(jobs_by_2rgn, file.path(dir_prep,'hotel_jobs_right_tmp.csv'), row.names=F, na='')
+# by hand save hotel_jobs_right.csv, which includes real data for Tel Aviv[rgn_id=6], Center[4], and Haifa Bay[3], and no data for the other 3 regions. Will have to think of a good way to modify these 3 regions, but it is a better starting point than option 1. 
+
+
+
+
---
+++ med2014/prep_layers/prep_le_layers.R
@@ -0,0 +1,29 @@
+# prep_le_layers.r
+
+# setup
+library(dplyr)
+library(tidyr) # install.packages('tidyr')
+dir_prep = 'prep_layers' # this folder is within ohi-israel/med2014
+
+
+# run prep_le_hotels.r
+source('prep_layers/prep_le_hotels.r')
+
+# run prep_le_aqua.r
+source('prep_layers/prep_le_aqua.r')
+
+
+
+## save jobs and wages layers
+
+f_corrected = read.csv(file.path(dir_prep,'hotel_jobs_right.csv')) # ensure this is the appropriate file. 
+
+write.csv(f_corrected %>%
+            select(rgn_id, year, count = hotel_jobs_n), 
+          file.path('layers', 'le_
+
+
+
+
+
+# 
