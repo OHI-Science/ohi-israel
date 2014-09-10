c2ea3e9aa87fb8aa7c974b3bb049e4236c2f2498
jules32 <jstewart@nceas.ucsb.edu>
2014-09-10 16:48:43 -0700

    successful run with new resilience layers by Katie

--- med2014/calculate_scores.R
+++ med2014/calculate_scores.R
@@ -1,5 +1,8 @@
-library(devtools)
-load_all('~/github/ohicore')
+# called from launch_ohi-israel.r. 
+
+# ## to run stand-alone:
+# library(devtools)
+# load_all('~/github/ohicore')
 #devtools::install('~/github/ohicore')
 #suppressWarnings(library(ohicore))
 
@@ -8,8 +11,6 @@ setwd('~/github/ohi-israel/med2014')
 
 # load conf
 conf = Conf('conf')
-
-#cat(sprintf('DEBUG calculate_scores.R:32, fxn environment summarize: %s\n', environmentName(environment(summarize)) ))
 
 # run checks on layers
 CheckLayers('layers.csv', 'layers', flds_id=conf$config$layers_id_fields)
