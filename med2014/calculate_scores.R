9e22e18c5f9ee9854bc13cbc4d8adb17b43e4edc
jules32 <jstewart@nceas.ucsb.edu>
2014-12-19 13:41:12 -0800

    update scores with Anat's edits

--- med2014/calculate_scores.R
+++ med2014/calculate_scores.R
@@ -17,3 +17,5 @@ layers = Layers('layers.csv', 'layers')
 # calculate scenario scores
 scores = CalculateAll(conf, layers, debug=F)
 write.csv(scores, 'scores.csv', na='', row.names=F)
+
+source('report.r')
