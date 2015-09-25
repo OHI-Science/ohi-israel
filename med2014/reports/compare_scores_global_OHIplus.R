a3a0a87790f50adb8236ec41536e2b6995ca77ea
jules32 <jstewart@nceas.ucsb.edu>
2015-09-25 16:21:59 -0700

    updating reporting ohi-israel

--- med2014/reports/compare_scores_global_OHIplus.R
+++ med2014/reports/compare_scores_global_OHIplus.R
@@ -8,7 +8,7 @@ library(RColorBrewer)  # install.packages('RColorBrewer')
 library(hwriter)       # install.packages('hwriter')
 
 # working directory filepath
-wd  = '~/github/ohi-israel/med2014'
+wd  = '~/github/chn/province2015'
 
 
 ## read in global/eez2014 scores ----
@@ -16,20 +16,20 @@ wd  = '~/github/ohi-israel/med2014'
 v2014.3_url = 'https://raw.githubusercontent.com/OHI-Science/ohi-global/4da6b4a1d69d694264ea68456359a939b0c03f9c/eez2014/scores.csv'
 scores_v2014.3 = read.csv(v2014.3_url)
 
-# israel's region_id in the global assessment is 79
-scores_global_israel = scores_v2014.3 %>%            
-  filter(region_id == 79) %>%
+# china's region_id in the global assessment is 209
+scores_global_china = scores_v2014.3 %>%            
+  filter(region_id == 209) %>%
   mutate(goal = as.character(goal))
 
 
-## read in ohi-israel/med2014 scores         
+## read in chn/province2015 scores         
 local_csv = file.path(wd, 'scores.csv')      
 scores_local = read.csv(local_csv) %>%
   mutate(goal = as.character(goal))
 
 
 ## join OHI+ and global scores ----
-scores_compare = scores_global_israel %>%   
+scores_compare = scores_global_china %>%   
   select(goal, 
          dimension,
          score_global = score) %>%         
@@ -40,16 +40,19 @@ scores_compare = scores_global_israel %>%
                      score_local = score), 
             by = c('goal', 'dimension')) %>%
   filter(dimension %in% c('future', 'score', 'status')) %>%
-  mutate(score_diff = score_global - score_local)
+  mutate(score_global = round(score_global, 1),
+         score_local  = round(score_local, 1),
+         score_diff = round(score_global - score_local, 1))
 
 
 ## make an html file that is color coded ----
 pal = brewer.pal(10, 'RdYlBu')
+pal = pal[1:9]
 
 # cols = data.frame(goal = scores_compare$goal)
 cols = scores_compare[, 'score_diff']
 cols = assign('score_diff', cut(cols, 
-                                 breaks=c(-100, -50, -30, -20, -10, 0, 10, 20, 30, 50, 100), 
+                                 breaks=c(-100, -50, -30, -20, -10, 0, 10, 20, 50, 100), 
                                  include.lowest = TRUE, labels=pal))
  
 hwrite(scores_compare, 
